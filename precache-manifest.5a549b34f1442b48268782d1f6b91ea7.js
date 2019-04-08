0881f590596e27a45a38275552034e36eca9fc9e
Abhishek K <abhishekakade@gmail.com>
2019-04-08 22:21:01 +0530

    Updates

---
+++ precache-manifest.5a549b34f1442b48268782d1f6b91ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "1af817009ff74a36bfda",
+    "url": "/calculator/static/js/main.336cfac4.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/calculator/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "1af817009ff74a36bfda",
+    "url": "/calculator/static/css/main.fa3ed8f1.chunk.css"
+  },
+  {
+    "revision": "693454627d05106eb8407dec402a4cd0",
+    "url": "/calculator/index.html"
+  }
+];
